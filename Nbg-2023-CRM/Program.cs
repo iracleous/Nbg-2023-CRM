7e695c51544fb73526563c381ddce38c073b725c
dimitris <iracleous@gmail.com>
2023-05-02 11:13:48 +0300

    new comment

--- Nbg-2023-CRM/Program.cs
+++ Nbg-2023-CRM/Program.cs
@@ -4,6 +4,7 @@ using Nbg_2023_CRM.Services;
 
 
 // comment
+// comment 2
 
 var customer1 = new Customer { Id = 2, Name = "Dimitris", Address = "Athens" };
 var customer2 = new Customer { Id = 2, Name = "Maria", Address = "Patra" };
